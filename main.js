8eecf546936df1912302d1212744355453cf87eb
Matt Haws <matthaws@gmail.com>
2018-03-31 09:09:56 -0400

    Refactor and reorganize

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("project-item").onmousemove = e => {
-      const x = e.pageX - e.target.offsetLeft;
-      const y = e.pageY - e.target.offsetTop;
-      e.target.style.setProperty("--x", `${x}px`);
-      e.target.style.setProperty("--y", `${y}px`);
-    };
-  });
+  document.querySelectorAll(".project-item").forEach(element => {
+    element.onmousemove = e => {
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
+    e.target.style.setProperty("--x", `${x}px`);
+    e.target.style.setProperty("--y", `${y}px`);
+  };
+});
+
+  document.querySelectorAll('#skills-title').forEach( element => {
+    const instance = basicScroll.create({
+      elem: element,
+      from: "bottom-bottom",
+      to: "top-middle",
+      props: {
+        "--font-size": {
+          from: '30px',
+          to: '50px',
+        }
+      }
+    });
+    instance.start();
+
+  })
+
+});
